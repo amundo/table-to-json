36b794f7bca12cfcbf3593af4ddb48ad5138f8fc
Patrick Hall <pathall@gmail.com>
2022-11-13 14:52:48 -0500

    Create table-to-json.js

---
+++ table-to-json.js
@@ -0,0 +1,46 @@
+let tableToJSON = (table, columnNames = null) => {
+  let trs = Array.from(table.querySelectorAll('tr'))
+
+  let columnHeaders = Array.from(trs[0].querySelectorAll('td,th'))
+    .map(cell => cell.textContent.trim())
+
+  if(columnNames){
+    columnHeaders = columnNames
+  }
+
+  let rowHeaders = trs
+    .slice(1) // first row is headers
+    .map(row => {
+      let cells = Array.from(row.querySelectorAll('td,th'))
+        .map(cell => cell.textContent.trim())
+
+      return cells[0]
+    })
+
+  let data = trs
+    .slice(1) // first row is column headers
+    .map(row => {
+      let cells = Array.from(row.cells)
+        .map(cell => cell.textContent.trim())
+
+      let entries = columnHeaders.map((_, i) => 
+        [columnHeaders[i], cells[i]]
+      )
+
+      let item = Object.fromEntries(entries)
+      return item
+    })
+
+  return {
+    rowHeaders,
+    columnHeaders,
+    data
+  }
+}
+
+export {
+  tableToJSON
+}
+
+  // document.designMode = 'on'
+  // console.table(table2json(document.querySelector('table')))
